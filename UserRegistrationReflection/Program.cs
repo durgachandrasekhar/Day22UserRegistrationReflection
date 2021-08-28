3e606798ad2541a9fc936bf2267a0c7b961645d4
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-28 13:16:16 +0530

    UserRegistrationUsingReflection

---
+++ UserRegistrationReflection/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistrationReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidateUserDetails validate = new ValidateUserDetails();
+            validate.ValidateUser();
+        }
+    }
+}
